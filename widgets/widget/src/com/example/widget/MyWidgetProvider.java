7e39b20fd61b74693d4d4ac9257b81557a3bcdac
cTn <ctndesigner@gmail.com>
2013-12-29 11:57:10 +0100

    up

--- widgets/widget/src/com/example/widget/MyWidgetProvider.java
+++ widgets/widget/src/com/example/widget/MyWidgetProvider.java
@@ -5,7 +5,6 @@ import java.util.Random;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -14,11 +13,9 @@ import android.widget.RemoteViews;
 public class MyWidgetProvider extends AppWidgetProvider {
 	  @Override
 	  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-	    // Get all ids
-	    ComponentName thisWidget = new ComponentName(context, MyWidgetProvider.class);
-	    int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-	    
-	    for (int widgetId : allWidgetIds) {
+	    for(int i = 0; i < appWidgetIds.length; i++){
+	      int widgetId = appWidgetIds[i];
+	      
 	      // create some random data
 	      int number = (new Random().nextInt(100));
 
