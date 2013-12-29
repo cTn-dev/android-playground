90537c75672b5e9672e490c9cd396838149d68c6
cTn <ctndesigner@gmail.com>
2013-12-29 12:21:09 +0100

    some doodling around log and onDelete

--- widgets/widget/src/com/example/widget/MyWidgetProvider.java
+++ widgets/widget/src/com/example/widget/MyWidgetProvider.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -13,14 +14,16 @@ import android.widget.RemoteViews;
 public class MyWidgetProvider extends AppWidgetProvider {
 	  @Override
 	  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-	    for(int i = 0; i < appWidgetIds.length; i++){
-	      int widgetId = appWidgetIds[i];
-	      
+	    // Get all ids
+	    ComponentName thisWidget = new ComponentName(context, MyWidgetProvider.class);
+	    int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+	    
+	    for (int widgetId : allWidgetIds) {
 	      // create some random data
 	      int number = (new Random().nextInt(100));
 
 	      RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-	      Log.w("WidgetExample", String.valueOf(number));
+	      Log.w("Random Number", String.valueOf(number));
 	      
 	      // Set the text
 	      remoteViews.setTextViewText(R.id.update, String.valueOf(number));
@@ -35,5 +38,15 @@ public class MyWidgetProvider extends AppWidgetProvider {
 	      remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
 	      appWidgetManager.updateAppWidget(widgetId, remoteViews);
 	    }
+	  }
+	  
+	  
+	  @Override
+	  public void onDeleted(Context context, int[] appWidgetIds) {
+		  Log.w("Widgets Deleted", String.valueOf(appWidgetIds.length));
+		  
+		  for (int i = 0; i < appWidgetIds.length; i++) {
+			  Log.w("Widget ID", String.valueOf(appWidgetIds[i]));  
+		  }
 	  }
 }
