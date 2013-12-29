773601d82fb458613966d303f0c8d730ed52744c
cTn <ctndesigner@gmail.com>
2013-12-29 12:45:21 +0100

    up

--- widgets/widget/src/com/example/widget/MyWidgetProvider.java
+++ widgets/widget/src/com/example/widget/MyWidgetProvider.java
@@ -5,48 +5,46 @@ import java.util.Random;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 public class MyWidgetProvider extends AppWidgetProvider {
-	  @Override
-	  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-	    // Get all ids
-	    ComponentName thisWidget = new ComponentName(context, MyWidgetProvider.class);
-	    int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-	    
-	    for (int widgetId : allWidgetIds) {
-	      // create some random data
-	      int number = (new Random().nextInt(100));
-
-	      RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-	      Log.w("Random Number", String.valueOf(number));
-	      
-	      // Set the text
-	      remoteViews.setTextViewText(R.id.update, String.valueOf(number));
-
-	      // Register an onClickListener
-	      Intent intent = new Intent(context, MyWidgetProvider.class);
-
-	      intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-	      intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-
-	      PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-	      remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
-	      appWidgetManager.updateAppWidget(widgetId, remoteViews);
+    @Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+	    for (int i = 0; i < appWidgetIds.length; i++) {
+	        int appWidgetId = appWidgetIds[i];
+	        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+	        
+	        // create some random data
+            int random_number = (new Random().nextInt(100));
+            
+            Log.w("Random Number", String.valueOf(random_number));
+              
+            // Set the text
+            remoteViews.setTextViewText(R.id.update, String.valueOf(random_number));
+            
+            // Register an onClickListener (why does this update all?)
+            Intent intent = new Intent(context, MyWidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
+            
+            // Update view
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
 	    }
-	  }
+    }
 	  
 	  
-	  @Override
-	  public void onDeleted(Context context, int[] appWidgetIds) {
-		  Log.w("Widgets Deleted", String.valueOf(appWidgetIds.length));
-		  
-		  for (int i = 0; i < appWidgetIds.length; i++) {
-			  Log.w("Widget ID", String.valueOf(appWidgetIds[i]));  
-		  }
-	  }
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        Log.w("Widgets Deleted", String.valueOf(appWidgetIds.length));
+      
+        for (int i = 0; i < appWidgetIds.length; i++) {
+            Log.w("Widget ID", String.valueOf(appWidgetIds[i]));  
+        }
+    }
 }
