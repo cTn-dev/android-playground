adda1d74e7e56e897f656fcb41cf7832fd7bcb79
cTn <ctndesigner@gmail.com>
2014-01-08 18:38:12 +0100

    more doodling

--- apps/openLRSngAndroidConfigurator/src/com/example/openlrsn_configurator/MainActivity.java
+++ apps/openLRSngAndroidConfigurator/src/com/example/openlrsn_configurator/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.openlrsn_configurator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
@@ -16,6 +19,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        
+        DrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerList = (ListView) findViewById(R.id.main_drawer);
+        
+        // UI hooks
+        DrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        
+        // Open drawer
+        DrawerLayout.openDrawer(GravityCompat.START);
         
         Log.d("onCreate", "Fired");
     }
@@ -28,5 +40,18 @@ public class MainActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
+    }
+    
+    /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+    
+    /* Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        
     }
 }
